1e740ada21d2c91c8f1f76a883f045990976d4f6
bzamanbd <riponzamanbd@gmail.com>
2022-02-12 17:33:08 +0600

    CRUD operation with firestore db is done.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,21 @@
+//step1:Creating firestore instance//
+// ignore_for_file: avoid_print
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_first_app/services/auth_service.dart';
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class HomeScreen extends StatelessWidget {
   final String title;
-  const HomeScreen({Key? key, required this.title}) : super(key: key);
+  HomeScreen({Key? key, required this.title}) : super(key: key);
+
+  //step2:Creating firestore instance//
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -22,8 +31,69 @@ class HomeScreen extends StatelessWidget {
                 style: TextButton.styleFrom(primary: Colors.white)),
           ],
         ),
-        body: const Center(
-          child: Text('this is homepage'),
+        body: Center(
+          child: SizedBox(
+            width: size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () async {
+                      //step3:Creating collectionReference//
+                      CollectionReference users = firestore.collection('users');
+                      //step4:Adding data in Map format(key:value)//
+                      await users.add({
+                        'name':'Radifa'
+                      });
+
+                      //to set your own docName//
+                      // await users.doc('fruit').set({'name': 'Mango'});
+                    },
+                    child: const Text('Add Data to Firestore')),
+                ElevatedButton(
+                    onPressed: () async {
+                      CollectionReference users = firestore.collection('users');
+                      //to get the output of users(collection)//
+                      // QuerySnapshot allResults = await users.get();
+                      // for (var result in allResults.docs) {
+                      //   print(result.data());
+                      // }
+
+                      //Read data from a specific doc of the collection of DB//
+                      DocumentSnapshot result = await users.doc('fruit').get();
+                      print(result.data());
+
+                      //realtiem data changing as stream//
+                      // users.doc('fruit').snapshots().listen((result) {
+                      //   print(result.data());
+                      // });
+                    },
+                    child: const Text('Read Data from DB')),
+
+                //to update data//
+                ElevatedButton(
+                    onPressed: () async {
+                      await firestore
+                          .collection('users')
+                          .doc('fruit')
+                          .update({
+                            'name':'Guava'
+                          });
+                    },
+                    child: const Text('Update Data')),
+
+                ElevatedButton(
+                    onPressed: () async {
+                      await firestore
+                          .collection('users')
+                          .doc('fruit')
+                          .delete();
+                    },
+                    child: const Text('Delete Data')),
+              ],
+            ),
+          ),
         ),
       ),
     );
